c9cbbb0cf0f20cb9b9e23a1c37f766085fd47619
Mingjia Shi <shimj9312@gmail.com>
2018-08-08 15:15:57 +0200

    create redux promise for api request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReduxPromise from 'redux-promise';
 import './index.css';
 import App from './components/app';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+    </Provider>, document.getElementById('root'));
